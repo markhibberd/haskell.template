93b37ccb4783287ae2be6b00a9ba6b236e82832a
Navin Keswani <navin@novemberkilo.com>
2018-01-29 12:00:09 +1100

    Cleanup - sets up License etc.

--- src/PROJECT_NAMESPACE_DIR.hs
+++ src/PROJECT_NAMESPACE_DIR.hs
@@ -1,12 +1,3 @@
--- |
--- Module:      PROJECT_NAMESPACE
--- Copyright:   (c) 2012 Mark Hibberd
--- License:     BSD3
--- Maintainer:  Mark Hibberd <mark@hibberd.id.au>
--- Portability: portable
---
--- PROJECT_DESC
---
 module PROJECT_NAMESPACE (module X) where
 
 import PROJECT_NAMESPACE.Core as X
